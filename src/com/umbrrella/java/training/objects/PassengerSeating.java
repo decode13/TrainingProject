e7edab4c9ac85f5b9e343c159f0c11b9beb9dea2
decode13 <1994448+decode13@users.noreply.github.com>
2018-03-18 22:41:06 +0200

    Trying to refactor code duplication.

--- src/com/umbrrella/java/training/objects/PassengerSeating.java
+++ src/com/umbrrella/java/training/objects/PassengerSeating.java
@@ -17,29 +17,25 @@ final public class PassengerSeating {
 		carSeatReservation(csvFile);
 		busSeatReservation(csvFile);
 		planeSeatReservation(csvFile);
-		
+
 	}
 
 	private PassengerSeating() {
 		// Nothing to do here.
 	}
-	
-	private static void carSeatReservation (final String csvFile) {
+
+	private static void carSeatReservation(final String csvFile) {
 		final StringBuffer separator = new StringBuffer();
 		final StringBuffer header = new StringBuffer(512);
 		String vehicleType = "Car"; // NOPMD on 18/03/18 18:50, with reason: LocalVariableCouldBeFinal
-
-		header.replace(0, header.length(), "");
-		header.append("Seating for vehicle type: " + vehicleType + ", model: ");
-		separator.replace(0, separator.length(), "");
 
 		final Car sedan = new Car();
 		sedan.setManufacturer("Volkswagen");
 		sedan.setModelNo("Passat");
 		sedan.plateNo = "C-AR-902813";
 		sedan.destination = "Munich";
-		header.append(sedan.getManufacturer() + " " + sedan.getModelNo() + ", plate number: " + sedan.plateNo
-				+ ", with destination: " + sedan.destination);
+		header.append("Seating for vehicle type: Car, model: " + sedan.getManufacturer() + " "
+				+ sedan.getModelNo() + ", plate number: " + sedan.plateNo + ", with destination: " + sedan.destination);
 		System.out.println(header); // NOPMD on 18/03/18 18:51, with reason: SystemPrintln
 		for (int h = 0; h < header.length(); h++) {
 			separator.append('-');
@@ -54,8 +50,8 @@ final public class PassengerSeating {
 		System.out.println(""); // NOPMD on 18/03/18 18:51, with reason: SystemPrintln
 
 	}
-	
-	private static void busSeatReservation (final String csvFile) {
+
+	private static void busSeatReservation(final String csvFile) {
 		final StringBuffer separator = new StringBuffer();
 		final StringBuffer header = new StringBuffer(512);
 		String vehicleType = "Bus"; // NOPMD on 18/03/18 18:50, with reason: LocalVariableCouldBeFinal
@@ -69,24 +65,24 @@ final public class PassengerSeating {
 		smallBus.setModelNo("Wheels");
 		smallBus.plateNo = "B-US-827643";
 		smallBus.destination = "Graz";
-		header.append(smallBus.getManufacturer() + " " + smallBus.getModelNo() + ", plate number: "
-		+ smallBus.plateNo + ", with destination: " + smallBus.destination);
+		header.append(smallBus.getManufacturer() + " " + smallBus.getModelNo() + ", plate number: " + smallBus.plateNo
+				+ ", with destination: " + smallBus.destination);
 		System.out.println(header); // NOPMD on 18/03/18 18:51, with reason: SystemPrintln
-		
+
 		for (int h = 0; h < header.length(); h++) {
-		separator.append('-');
+			separator.append('-');
 		}
 		System.out.println(separator); // NOPMD on 18/03/18 18:51, with reason: SystemPrintln
-		
+
 		final List<Passenger> passengerList = GetPassengerList.getFromCSV(csvFile);
 		for (int p = 0; p < passengerList.size(); p++) { // NOPMD on 18/03/18 18:51, with reason: LawOfDemeter
-		smallBus.reserveSeat(passengerList.get(p)); // NOPMD on 18/03/18 18:51, with reason: LawOfDemeter
+			smallBus.reserveSeat(passengerList.get(p)); // NOPMD on 18/03/18 18:51, with reason: LawOfDemeter
 		}
 		smallBus.listSeats();
 		System.out.println(""); // NOPMD on 18/03/18 18:51, with reason: SystemPrintln
 	}
 
-	private static void planeSeatReservation (final String csvFile) {
+	private static void planeSeatReservation(final String csvFile) {
 		final StringBuffer separator = new StringBuffer();
 		final StringBuffer header = new StringBuffer(512);
 		String vehicleType = "Plane"; // NOPMD on 18/03/18 18:50, with reason: LocalVariableCouldBeFinal
@@ -101,16 +97,16 @@ final public class PassengerSeating {
 		tinyPlane.plateNo = "DE-12319908";
 		tinyPlane.destination = "Paris";
 		header.append(tinyPlane.getManufacturer() + " " + tinyPlane.getModelNo() + ", plate number: "
-		+ tinyPlane.plateNo + ", with destination: " + tinyPlane.destination);
+				+ tinyPlane.plateNo + ", with destination: " + tinyPlane.destination);
 		System.out.println(header); // NOPMD on 18/03/18 18:51, with reason: SystemPrintln
 		for (int h = 0; h < header.length(); h++) {
-		separator.append('-');
+			separator.append('-');
 		}
 		System.out.println(separator); // NOPMD on 18/03/18 18:51, with reason: SystemPrintln
-		
+
 		final List<Passenger> passengerList = GetPassengerList.getFromCSV(csvFile);
 		for (int p = 0; p < passengerList.size(); p++) { // NOPMD on 18/03/18 18:51, with reason: LawOfDemeter
-		tinyPlane.reserveSeat(passengerList.get(p)); // NOPMD on 18/03/18 18:51, with reason: LawOfDemeter
+			tinyPlane.reserveSeat(passengerList.get(p)); // NOPMD on 18/03/18 18:51, with reason: LawOfDemeter
 		}
 		tinyPlane.listSeats();
 		System.out.println(""); // NOPMD on 18/03/18 18:51, with reason: SystemPrintln
